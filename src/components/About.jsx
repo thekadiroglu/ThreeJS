dfe3fe5efcd6b9a7621b9ef9891daa13a90bee82
thekadiroglu <116295346+thekadiroglu@users.noreply.github.com>
2023-09-30 16:57:05 +0300

    Initial commit

---
+++ src/components/About.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Tilt } from 'react-tilt'
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion'
+import { SectionWrapper } from '../hoc';
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div variants={fadeIn("right", "spring", 0.5 * index, 0.75)} className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'>
+        <div options={{ max: 45, scale: 1, speed: 450 }} className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'>
+          <img src={icon} alt={title} className='w-16 h-16 object-contain' />
+          <h3 className='text-white text-[20px] font-bold text-center' >{title}</h3>
+        </div>
+        
+      </motion.div>
+    </Tilt>
+  )
+}
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>
+          Introductıon
+        </p>
+        <h2 className='font-bold'>Oveirwiev.</h2>
+      </motion.div>
+      <motion.p variants={fadeIn("", "", 0.1, 1)} className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'>
+      As a 19-year-old, I love using a computer. That's exactly why I started my curiosity about software. I know how to use most applications, and I learn what I don't know right away. I love chatting with my friends, I am a friendly personality. I also love learning new things. I get busy with things like English, new software languages, web series, etc.
+      </motion.p>
+
+      <div className='mt-20 flex lex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default SectionWrapper(About, "about")
